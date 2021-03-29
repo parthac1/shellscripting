0db91279bf90cab6e372ba895087e66dcf1f10e6
parthastudent <72043891+parthastudent@users.noreply.github.com>
2021-03-29 10:40:48 -0700

    Thi is  ver1

---
+++ calulate.sh
@@ -0,0 +1,15 @@
+echo "Please Etner a number "
+read number1
+echo "Please Enter another number "
+read number2 
+
+addition=$number1+$number2
+multiplication=$number1*$number2
+
+
+echo "Addition of the 2 numbers are $addition"
+
+echo " multiplication of the 2 numbers are $multiplication"
+
+
+
