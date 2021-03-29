38f84995051b6bfa4aaa1c833db6d178260bb537
parthastudent <72043891+parthastudent@users.noreply.github.com>
2021-03-29 11:02:36 -0700

    Thi is  ver2

--- calulate.sh
+++ calulate.sh
@@ -3,8 +3,8 @@ read number1
 echo "Please Enter another number "
 read number2 
 
-addition=$number1+$number2
-multiplication=$number1*$number2
+addition=$(($number1+$number2))
+multiplication=$(($number1*$number2))
 
 
 echo "Addition of the 2 numbers are $addition"
